ec5c72a6818ae374da0df74766e84e57da37b629
majewing <majewing@126.com>
2016-03-03 17:04:58 +0800

    remove some invalid codes

--- motion-header-viewpager/src/main/java/com/maje/mhvp/TouchPanelLayout.java
+++ motion-header-viewpager/src/main/java/com/maje/mhvp/TouchPanelLayout.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.os.Build;
 import android.os.SystemClock;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
